4a1c91c67c12965494f03afbc2749a66856ba641
Nikita Noskov <noskovnikita@lenta.ru>
2024-05-17 13:15:18 +0300

    Homework

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,11 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
